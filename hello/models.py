e5c5921a5225250c8469a162917fe20cbb4d840a
hirose-takashi <hirose.tabo@gmail.com>
2018-11-30 23:37:22 +0100

    coffeecalc

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
