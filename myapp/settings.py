152d735ba7d0bb36ad4f2e9044123407aa50b9dc
hirose-takashi <hirose.tabo@gmail.com>
2018-12-01 10:57:46 +0100

    coffeecalc

--- myapp/settings.py
+++ myapp/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',    # 追加した
 )
 
 ROOT_URLCONF = 'myapp.urls'
