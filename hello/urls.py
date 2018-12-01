4eee0b5ce4c9b188d0694aca358cd8521378ec74
hirose-takashi <hirose.tabo@gmail.com>
2018-12-01 10:34:43 +0100

    coffeecalc

--- hello/urls.py
+++ hello/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^$', views.hello_world, name='hello_world'),
-    # url(r'^template/$', views.hello_template, name='hello_template'),
-    # url(r'^if/$', views.hello_if, name='hello_if'),
-    # url(r'^for/$', views.hello_for, name='hello_for'),
-    # url(r'^get/$', views.hello_get_query, name='hello_get_query'),
-    # url('/hello/', views.hello_form_post, name='hello_form_post'),
-    url(r'^$', views.hello_get_query, name='hello_get_query'),
+    url(r'^$', views.hello_world, name='hello_world'),
+    url(r'^template/$', views.hello_template, name='hello_template'),
+    url(r'^if/$', views.hello_if, name='hello_if'),
+    url(r'^for/$', views.hello_for, name='hello_for'),
+    url(r'^get/$', views.hello_get_query, name='hello_get_query'),
+    url('/hello/', views.hello_form_post, name='hello_form_post'),
+
 ]
